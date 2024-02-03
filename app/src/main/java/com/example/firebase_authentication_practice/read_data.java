ae27517dc62598aa54c3f89fa1d54346131f31be
Sourabh9041 <kapoorsourabh501@gmail.com>
2024-02-03 15:41:21 +0530

    Change in on cancelled

--- app/src/main/java/com/example/firebase_authentication_practice/read_data.java
+++ app/src/main/java/com/example/firebase_authentication_practice/read_data.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -63,6 +64,7 @@ public class read_data extends AppCompatActivity {
                     @Override
                     public void onCancelled(@NonNull DatabaseError error) {
 
+                        Toast.makeText(read_data.this, "Error getting The Data", Toast.LENGTH_SHORT).show();
                     }
                 });
 
